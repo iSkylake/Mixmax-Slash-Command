badad474147f50c59c03ee5786afedc196c8f251
Llorch <SkylakeBeta@gmail.com>
2016-12-16 23:50:25 -0800

    Add certificates, server, api and js files

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var sync = require('synchronize');
+var cors = require('cors');
+var fs = require('fs');
+var https = require('https');
+
+var options = {
+   key  : fs.readFileSync('./certificates/server.key'),
+   cert : fs.readFileSync('./certificates/server.crt')
+};
+
+// Use fibers in all routes so we can use sync.await() to make async code easier to work with.
+app.use(function(req, res, next) {
+  sync.fiber(next);
+});
+
+// Since Mixmax calls this API directly from the client-side, it must be whitelisted.
+var corsOptions = {
+  origin: /^[^.\s]+\.mixmax\.com$/,
+  credentials: true
+};
+
+
+app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
+app.get('/resolver', cors(corsOptions), require('./api/resolver'));
+
+app.listen(process.env.PORT || 9145);
+
+https.createServer(options, app).listen(3000, function () {
+   console.log('Started!');
+});
